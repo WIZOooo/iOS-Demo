77bb3468ea23fc5ee85af5bea33d1a926e882034
Albz <>
2022-08-10 17:39:39 +0800

    【多线程示例】补充队列处理混合任务的情况

--- iOS Demo/Foundation/MultiThreadViewController.m
+++ iOS Demo/Foundation/MultiThreadViewController.m
@@ -14,19 +14,21 @@
 @implementation MultiThreadViewController
 
 /*
- 可以把队列看成是处理机构，机构中的角色都有两个，处理人和分配人。
- 串行队列是机构中只有一个人身兼两职
- 并行队列是机构中有很多处理人,还有一个分配人。
+ 可以把队列看成是处理机构，机构中人员都有两个职能，处理和分配。
+ 串行队列是机构中只有一个人
+ 并行队列是机构中有很多人。
  
- 任务可以看成是带有字条的办事者，字条就是任务的具体内容，
- 同步任务是分配人必需把办事者的纸条念完才能处理别的办事者，
- 异步任务是可以让分配人先登记，稍后处理人再念字条的内容
+ 任务可以看成是带有字条的办事者
+ 同步任务是工作人员人必需把办事者的纸条念完才能处理别的任务，
+ 异步任务是可以让工作人员先分配，然后再由其他人员或者当前人员来执行（取决于有几个人）。
  
- 当串行队列遇到同步任务，处理兼职分配人直接念字条，先进先出
- 当串行队列遇到异步任务，处理兼职分配人登记，先进先出，处理兼职分配人念字条，先进先出
+ 当串行队列遇到任何任务，工作人员直接按照任务顺序办事，
+ 如果任务中还有子任务，
+ 如果子任务是同步任务，死锁，因为队列需要执行完当前任务才能添加子任务，但是子任务的添加需要等待队列执行完当前任务。
+ 如果子任务是异步任务，排在队列最后执行。
  
- 当并发队列遇到同步任务，分配人直接念字条，先进先出
- 当并发队列遇到异步任务，分配人登记给不同处理人，各个处理人同时执行
+ 当并发队列遇到同步任务，工作人员直接办事
+ 当并发队列遇到异步任务，工作人员直接分给其他工作人员，各工作人员同时执行。
  */
 
 - (void)viewDidLoad {
@@ -34,7 +36,11 @@
     
     NSLog(@"主线程 %@",[NSThread currentThread]);
     
-    [self serialQueueTest];
+    [self serialQueueTestMixedTaskTest];
+    
+//    [self conCurrentQueueTestMixedTaskTest];
+    
+//    [self serialQueueTest];
     
 //    [self conCurrentQueueTest];
 }
@@ -79,6 +85,69 @@
     
     // 添加同步任务，结果是主线程执行，顺序执行任务
 //    [self addSyncTask:concurrent_queue];
+}
+
+
+// 先异步后同步
+- (void)asyncAndSync:(dispatch_queue_t)queue {
+    ///添加异步任务
+    ///添加同步任务，同步任务执行时，异步任务中再添加异步任务
+    ///查看异步任务是否执行，验证同步任务是否会阻塞队列
+    
+    dispatch_async(queue, ^{
+        NSLog(@"异步任务执行");
+        [NSThread sleepForTimeInterval: 3];
+        NSLog(@"添加异步子任务");
+        dispatch_async(queue, ^{
+            NSLog(@"异步子任务执行");
+        });
+    });
+    
+    dispatch_sync(queue, ^{
+        NSLog(@"同步任务执行");
+//        [NSThread sleepForTimeInterval: 10];
+//        NSLog(@"同步任务执行完毕");
+    });
+}
+
+// 先同步后异步
+- (void)syncAndAsync:(dispatch_queue_t)queue {
+    ///添加同步任务
+    ///添加异步任务，同步任务执行时，异步任务中再添加异步任务
+    ///查看异步任务是否执行，验证同步任务是否会阻塞队列
+    dispatch_sync(queue, ^{
+        NSLog(@"同步任务执行");
+        [NSThread sleepForTimeInterval: 10];
+        NSLog(@"同步任务执行完毕");
+    });
+    
+    dispatch_async(queue, ^{
+        NSLog(@"异步任务执行");
+        [NSThread sleepForTimeInterval: 3];
+        NSLog(@"添加异步子任务");
+        dispatch_async(queue, ^{
+            NSLog(@"异步子任务执行");
+        });
+    });
+}
+
+// 并发队列里面的混合任务（同步+异步）
+- (void)conCurrentQueueTestMixedTaskTest {
+    dispatch_queue_t concurrent_queue = dispatch_queue_create("DanCONCURRENT", DISPATCH_QUEUE_CONCURRENT);
+    ///结果是：同步任务执行中时，异步任务及其子任务均执行完毕
+//    [self asyncAndSync:concurrent_queue];
+    
+    
+    ///结果是：同步任务执行中时，异步任务及其子任务均不执行
+    [self syncAndAsync:concurrent_queue];
+}
+
+- (void)serialQueueTestMixedTaskTest {
+    dispatch_queue_t serialQueue = dispatch_queue_create("Dan-serial", DISPATCH_QUEUE_SERIAL);
+    
+    // 结果是：异步任务-同步任务-异步子异步任务
+    [self asyncAndSync:serialQueue];
+    
 }
 
 @end
